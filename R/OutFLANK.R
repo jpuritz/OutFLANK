c90b41db486dd5574826745aef4db322f1953618
Jon Puritz <jpuritz@uri.edu>
2025-06-22 16:11:56 -0400

    Update OutFLANK.R

--- R/OutFLANK.R
+++ R/OutFLANK.R
@@ -320,7 +320,7 @@ pOutlierFinderChiSqNoCorr=function(DataList, Fstbar, dfInferred, qthreshold=0.05
   pList = pTwoSidedFromChiSq(DataListGood$FSTNoCorr*(dfInferred)/Fstbar,dfInferred)
   pListRightTail = 1-pchisq(DataListGood$FSTNoCorr*(dfInferred)/Fstbar,dfInferred)
   
-  qtemp=qvalue(pListRightTail,fdr.level=qthreshold)
+  qtemp=qvalue(pListRightTail,fdr.level=qthreshold, pi0=1)
   #Note:  Using the bootstrap method here seems OK, but if this causes problems remove the pi0.method="bootstrap" in the previous line to revert to the default.
   #Puritz edit: I removed the pi0.method="bootstrap" method
  
